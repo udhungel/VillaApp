d02b62505071fc163549886ff4ea67f54be565b4
udhungel <udeepdhungel@gmail.com>
2022-12-27 22:22:52 +0100

    Consuming API - Added Auto Mapper

---
+++ MagicVilla_WebApp/AutoMapper/MappingConfig.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using MagicVilla_WebApp.Models;
+using MagicVilla_WebApp.Models.Dto;
+
+namespace MagicVilla_WebApp.AutoMapper
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig() 
+        {
+            CreateMap<VillaDTO, VillaCreateDTO>().ReverseMap();
+            CreateMap<VillaDTO, VillaUpdateDTO>().ReverseMap();
+            
+
+            CreateMap<VillaNumberDTO, VillaNumberCreateDTO>().ReverseMap();
+            CreateMap<VillaNumberDTO, VillaNumberUpdateDTO>().ReverseMap();
+
+
+        }
+    }
+}
--- MagicVilla_WebApp/Program.cs
+++ MagicVilla_WebApp/Program.cs
@@ -1,7 +1,10 @@
+using MagicVilla_WebApp.AutoMapper;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddAutoMapper(typeof(MappingConfig));
 
 var app = builder.Build();
 
